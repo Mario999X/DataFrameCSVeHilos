94a520cd5c274c2d1ed82f4d90424233729e823c
Mario <mariogr901@gmail.com>
2022-09-29 00:07:01 +0200

    Agregada la comprobacion del directorio.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,22 @@
 import controllers.ControllerAccidentes
 import controllers.ControllerProducts
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 
 fun main() {
 
     println("Ejecutando App \n")
 
+    // Comprobacion del directorio
+    val workingDirectory: String = System.getProperty("user.dir")
+    val path = Paths.get(workingDirectory + File.separator + "data")
+
+    val isDir = Files.isDirectory(path)
+
+    if (!isDir) Files.createDirectory(path)
+
+    // Ejecucion de los controladores
     ControllerProducts.procesarCopiaProductos()
     ControllerAccidentes.procesarCopiasAccidentes()
 
